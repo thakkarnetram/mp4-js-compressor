961d4572a6fc90b5ba6d0b7775beaf4a212493c4
thakkarnetram <thakkarnetram10@gmail.com>
2025-12-09 14:15:26 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -13,6 +13,23 @@ ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 const app = express();
 app.use(cors());
+const allowedOrigins = [
+  'https://tinycompression.netlify.app',
+];
+app.use(cors({
+  origin: function(origin, callback){
+
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization','Accept'],
+  credentials: true 
+}));
 app.use(express.json());
 
 // mount routers (each router will apply its own multer)
