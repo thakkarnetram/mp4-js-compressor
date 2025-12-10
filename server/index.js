115ac30afff4742d25530698f6a5cab1a02b9236
thakkarnetram <thakkarnetram10@gmail.com>
2025-12-10 16:02:31 +0530

    cors problem debug #3

--- server/index.js
+++ server/index.js
@@ -36,7 +36,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('/*', cors(corsOptions));
 app.use(express.json());
 
 // mount routers (each router will apply its own multer)
